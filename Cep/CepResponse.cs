da1d6578c70425b5a1b1c6f7843d949bcb9beb6e
Luffy <diegofernandeslins@gmail.com>
2021-07-22 13:24:02 -0300

    Add project files.

---
+++ Cep/CepResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cep
+{
+    class CepResponse
+    {
+        public string Cep { get; set; }
+        public string Logradouro { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
