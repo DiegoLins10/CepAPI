74a99bda3d1b41af8628744b61ae4b71eae826c1
Luffy <diegofernandeslins@gmail.com>
2021-07-22 14:08:26 -0300

    Entities da API

---
+++ Cep/Entities/CepResponse.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace Cep.Entities
+{
+    class CepResponse
+    {
+        /*
+         * Nosso objeto onde vai armazenar os atributos do Json
+         * E nele usamos o Jsonproperty para vincular os atributos com o Json
+         */
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+        [JsonProperty("localidade")]
+        public string Localidade { get; set; }
+        [JsonProperty("uf")]
+        public string Uf { get; set; }
+        [JsonProperty("unidade")]
+        public string Unidade { get; set; }
+        [JsonProperty("ibge")]
+        public string Ibge { get; set; }
+        [JsonProperty("gia")]
+        public string Gia { get; set; }
+    }
+}
