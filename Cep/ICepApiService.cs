f175c54f79da1446ce458636d6968ab4efcbbac7
Luffy <diegofernandeslins@gmail.com>
2021-07-22 14:08:54 -0300

    Criação do metodo task para chamar a API

---
+++ Cep/ICepApiService.cs
@@ -0,0 +1,16 @@
+﻿
+using Cep.Entities;
+using Refit;
+using System.Threading.Tasks;
+
+namespace Cep
+{/*
+  * Uma interface onde criamos uma task para fazer o request a API
+  */
+    interface ICepApiService
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> GetAddressAssync(string cep);
+        
+    }
+}
--- Cep/Program.cs
+++ Cep/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
 
 namespace Cep
 {
     class Program
-    {
-        static void Main(string[] args)
+    {/*
+      * Uma classe Task assincrona para esperar a informação da API
+      */
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Solicitando ao usuario o Cep
+             * e chamando o metodo task para iniciar o chamado a API
+             */
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.Write("Informe seu CEP: ");
+                string cepInformado = Console.ReadLine();
+                Console.WriteLine("Consultando informações do CEP: " + cepInformado);
+
+                var address = await cepClient.GetAddressAssync(cepInformado);
+
+                Console.WriteLine("Logradouro: " + address.Logradouro);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Erro na consulta de cep: " + e.Message);
+            }
         }
     }
 }
